a986793cec19e50d7f8f4ff4d5c53f5296355392
Cliff Sharp <cliff@csharpTech.net>
2020-03-20 13:07:39 -0500

    Initial Commit

---
+++ JenkinsTestProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkinsTestProj
+//
+//  Created by Cliff Sharp on 3/20/20.
+//  Copyright © 2020 csharpTechNet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
